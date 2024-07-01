ea93117f550f9894cfc3a3a2fa046d42c580b6c1
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-02 01:10:48 +0200

    moved game

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ExampleBox from '@/components/ExampleBox.vue'
-import ExampleLuckyNameGame from '@/examples/ExampleLuckyNameGame.vue'
+import ExampleLuckyNameGame from '@/components/ExampleLuckyNameGame.vue'
 </script>
 
 <template>
